9fb7052d1fbc4a6886f333f53067a8447e7372e3
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-11-12 20:25:03 +1100

    changed the etl.py

--- etl.py
+++ etl.py
@@ -6,11 +6,12 @@ from pyspark.sql.functions import udf, col, monotonically_increasing_id
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format, dayofweek
 from pyspark.sql.types import *
 
+
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID'] = config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -30,22 +31,22 @@ def process_song_data(spark, input_data, output_data):
     :param input_data: input file path
     :param output_data: output file path
     """
-
+    
     # get filepath to song data file
     song_data = input_data + "song_data/*/*/*/*"
-
+    
     # read song data file
     df = spark.read.json(song_data, mode='PERMISSIVE', columnNameOfCorruptRecord='corrupt_record').drop_duplicates()
 
     # extract columns to create songs table
     songs_table = df.select("song_id","title","artist_id","year","duration").drop_duplicates()
-
+    
     # write songs table to parquet files partitioned by year and artist
     songs_table.write.parquet(output_data + "songs/", mode="overwrite", partitionBy=["year","artist_id"])
 
     # extract columns to create artists table
     artists_table = df.select("artist_id","artist_name","artist_location","artist_latitude","artist_longitude").drop_duplicates()
-
+    
     # write artists table to parquet files
     artists_table.write.parquet(output_data + "artists/", mode="overwrite")
 
@@ -59,7 +60,7 @@ def process_log_data(spark, input_data, output_data):
     :param input_data: input file path
     :param output_data: output file path
     """
-
+    
     # get filepath to log data file
     log_data = os.path.join(input_data, "log-data/")
 
@@ -99,7 +100,9 @@ def process_log_data(spark, input_data, output_data):
 
     # extract columns from joined song and log datasets to create songplays table
     songplays_table = df.join(song_df, df.song == song_df.title, how='inner')\
-                        .select(monotonically_increasing_id().alias("songplay_id"),col("start_time"),col("userId").alias("user_id"),"level","song_id","artist_id", col("sessionId").alias("session_id"), "location", col("userAgent").alias("user_agent"))
+                        .select(monotonically_increasing_id().alias("songplay_id"),col("start_time"),\
+                                col("userId").alias("user_id"),"level","song_id","artist_id",\
+                                col("sessionId").alias("session_id"), "location", col("userAgent").alias("user_agent"))
 
     songplays_table = songplays_table.join(time_table, songplays_table.start_time == time_table.start_time, how="inner")\
                         .select("songplay_id", songplays_table.start_time, "user_id", "level", "song_id", "artist_id", "session_id", "location", "user_agent", "year", "month")
